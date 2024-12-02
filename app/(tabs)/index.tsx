047f81e75a127bc299689bc9f8c4a0629e3c21e6
Robert Sajdok <robert.sajdok@gmail.com>
2024-12-02 23:33:10 +0100

    initial commit

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,11 +1,124 @@
 import { Image, StyleSheet, Platform } from 'react-native';
 
-import { HelloWave } from '@/components/HelloWave';
 import ParallaxScrollView from '@/components/ParallaxScrollView';
 import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
+import { fetch } from 'expo/fetch';
+
+import * as Location from 'expo-location';
+import { useEffect, useState } from 'react';
+
+type Address = {
+  place_id: number;
+  osm_type: string;
+  osm_id: number;
+  type: string;
+  name: string;
+  addressType: string;
+};
+
+type Details = {
+  place_id: number;
+  osm_type: string;
+  osm_id: number;
+  category: string;
+  type: string;
+  localname: any;
+  extratags: ExtraTags;
+}
+
+type ExtraTags = {
+  maxspeed: string;
+}
+
 
 export default function HomeScreen() {
+  const [location, setLocation] = useState<Location.LocationObject | null>(null);
+  const [errorMsg, setErrorMsg] = useState<string | null>(null);
+  const [speed, setSpeed] = useState<number | null>(0);
+  const [address, setAddress] = useState<Address | null>(null);
+  const [details, setDetails] = useState<Details | null>(null);
+
+  useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        setErrorMsg('Permission to access location was denied');
+        return;
+      }
+      Location.watchPositionAsync({ accuracy: Location.Accuracy.High, timeInterval: 1000, distanceInterval: 1, }, async (loc) => {
+        if (location?.coords.longitude == loc.coords.longitude && location?.coords.latitude == loc.coords.latitude) {
+          console.log('No change in location');
+          return;
+        }
+
+        setLocation(loc);
+        if (loc.coords.speed) {
+          setSpeed(loc.coords.speed * 3.6); // Convert m/s to km/h 
+        }
+        console.log(location?.coords.longitude.toFixed(9), location?.coords.latitude.toFixed(9));
+        console.log(speed?.toFixed(2));
+
+        // Tests only
+        // https://nominatim.openstreetmap.org/reverse.php?lat=49.881243&lon=19.4889423&format=json&zoom=17
+        // https://nominatim.openstreetmap.org/details.php?osmtype=W&osmid=207877134&addressdetails=0&hierarchy=0&group_hierarchy=1&format=json
+        // https://www.openstreetmap.org/way/207877134
+
+        try {
+          // Address
+          const addressPath = `https://nominatim.openstreetmap.org/reverse.php?lat=${loc?.coords.latitude}&lon=${loc?.coords.longitude}&format=json&zoom=17`;
+          console.log(addressPath);
+
+          const addressResponse = await fetch(addressPath,
+            {
+              method: 'GET',
+              headers: { 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:92.0) Gecko/20100101 Firefox/92.0', 'Accept': 'application/json' }
+            }
+          );
+          console.log(addressResponse.status);
+
+          const addressData = await addressResponse.json();
+
+          if (addressData.osm_id == address?.osm_id) {
+            console.log('No change in address');
+            return;
+          }
+
+          setAddress(addressData);
+          console.log(address?.osm_id);
+          console.log(address?.name);
+          console.log(address?.osm_type);
+          console.log(address?.type);
+
+
+          // Details
+          // 207877134
+          const id = addressData.osm_id;
+          const detailsPath = `https://nominatim.openstreetmap.org/details.php?osmtype=W&osmid=${id}&addressdetails=0&hierarchy=0&group_hierarchy=1&format=json`;
+          const detailsResponse = await fetch(detailsPath,
+            {
+              method: 'GET',
+              headers: { 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:92.0) Gecko/20100101 Firefox/92.0', 'Accept': 'application/json' }
+            }
+          );
+
+          console.log(detailsResponse.status);
+          const details = await detailsResponse.json();
+          console.log(JSON.stringify(details));
+          setDetails(details);
+          console.log(details.place_id);
+          console.log(details.osm_id);
+          console.log(details.localname);
+          console.log(JSON.stringify(details.extratags));
+        } catch (error) {
+          console.error(error);
+          setErrorMsg(String(error));
+        }
+      });
+    }
+    )();
+  }, []);
+
   return (
     <ParallaxScrollView
       headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
@@ -15,40 +128,12 @@ export default function HomeScreen() {
           style={styles.reactLogo}
         />
       }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({
-              ios: 'cmd + d',
-              android: 'cmd + m',
-              web: 'F12'
-            })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          Tap the Explore tab to learn more about what's included in this starter app.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          When you're ready, run{' '}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
+      <ThemedView>
+        <ThemedText>{speed?.toFixed(2)}</ThemedText>
+        <ThemedText>{location?.coords.longitude.toFixed(9)} {location?.coords.latitude.toFixed(9)}</ThemedText>
+        <ThemedText>{address?.name} {address?.osm_type} {address?.type}</ThemedText>
+        <ThemedText>{details?.localname} {JSON.stringify(details?.extratags)}</ThemedText>
+        <ThemedText>{errorMsg}</ThemedText>
       </ThemedView>
     </ParallaxScrollView>
   );
